4642ebfbe76dffeb9d1e8ea206edf0e3741a2338
danytse <73063926+danytse@users.noreply.github.com>
2024-03-05 23:15:57 +0300

    feat: Change ports

--- all-frontend/src/configs/Config.js
+++ all-frontend/src/configs/Config.js
@@ -1,12 +1,9 @@
 export const headers = {
-    Accept: 'application/xml',
-    'Content-Type': 'application/xml',
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Credentials': "true"
 }
 
 export const urls = [
     "http://localhost:8080/organization-service",
-    "http://localhost:8085",
+    "http://localhost:8079",
 ]
